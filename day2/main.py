e45e7df6ad9753b57067bda2ce8288095896d6ff
Wes Barnett <wes@barnettphd.com>
2021-12-02 08:54:28 -0500

    day 2 part 2

--- day2/main.py
+++ day2/main.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
+lines = Path("./input").read_text().rstrip().split("\n")
+
+# Part 1
 position = 0
 depth = 0
-
-lines = Path("./input").read_text().rstrip().split("\n")
 
 for x in lines:
     cmd, amt = x.split(" ")
@@ -14,5 +15,23 @@ for x in lines:
         depth += amt
     elif cmd == "up":
         depth -= amt
+
+print(depth*position)
+
+# Part 2
+position = 0
+depth = 0
+aim = 0
+
+for x in lines:
+    cmd, amt = x.split(" ")
+    amt = int(amt)
+    if cmd == "down":
+        aim += amt
+    elif cmd == "up":
+        aim -= amt
+    elif cmd == "forward":
+        position += amt
+        depth += aim*amt
 
 print(depth*position)
