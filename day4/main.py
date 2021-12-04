af9daaa3f12b5d2a41b7ae978e664790d9cf53f2
Wes Barnett <wes@barnettphd.com>
2021-12-04 07:59:01 -0500

    day 4 part 2

--- day4/main.py
+++ day4/main.py
@@ -13,30 +13,43 @@ class Card:
                 self.unmarked_nums.add(int(y))
 
         self.marked = [[False for _ in x.split()] for x in rows]
+        self.won = False
 
     def play(self, num):
         if num in self.data:
             x, y = self.data[num]
             self.marked[x][y] = True
+        if num in self.unmarked_nums:
             self.unmarked_nums.remove(num)
         return self.win()
 
     def win(self):
         for row in self.marked:
             if all(row):
+                self.won = True
                 return True
         for c in range(len(self.marked[0])):
             if all([self.marked[r][c] for r in range(len(self.marked[0]))]):
+                self.won = True
                 return True
         return False
 
 
-def play_bingo(cards, plays):
+def part1(cards, plays):
     for play in plays:
         for card in cards:
             result = card.play(play)
             if result:
                 return card, play
+
+def part2(cards, plays):
+    for play in plays:
+        for card in cards:
+            if not card.won:
+                result = card.play(play)
+                if result:
+                    winner = card, play
+    return winner
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -48,6 +61,9 @@ if __name__ == "__main__":
     plays = [int(x) for x in lines[0].split(",")]
     cards = [Card(x) for x in lines[1:]]
 
-    winner, play = play_bingo(cards, plays)
+    winner, play = part1(cards, plays)
+    print(sum(list(winner.unmarked_nums))*play)
+
+    winner, play = part2(cards, plays)
     print(sum(list(winner.unmarked_nums))*play)
 
