4e63eaae64b25c17a06c06a91d37d0a7c2ec4041
Wes Barnett <wes@barnettphd.com>
2021-12-06 08:04:26 -0500

    fix day6 part 1, add part 2

--- day6/main.py
+++ day6/main.py
@@ -1,22 +1,28 @@
 from argparse import ArgumentParser
+from collections import Counter
 from pathlib import Path
+
+
+def calc_num_fish(fish, days):
+
+    for _ in range(days):
+        fish_new = Counter()
+        for i in range(8, 0, -1):
+            fish_new[i-1] = fish[i]
+
+        fish_new[8] = fish[0]
+        fish_new[6] += fish[0]
+        fish = fish_new
+
+    return sum(fish.values())
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-i", "--infile", required=False, type=Path, default=Path("input"))
     args = parser.parse_args()
 
-    fish = [int(x) for x in Path(args.infile).read_text().rstrip("\n").split(",")]
+    fish = Counter([int(x) for x in Path(args.infile).read_text().rstrip("\n").split(",")])
 
-    days = 80
-
-    for _ in range(days):
-        fish_new = list(fish)
-        for i, x in enumerate(fish):
-            fish_new[i] -= 1
-            if fish_new[i] == -1:
-                fish_new[i] = 6
-                fish_new.append(8)
-        fish = list(fish_new)
-
-    print(len(fish))
+    print(calc_num_fish(fish, 80))
+    print(calc_num_fish(fish, 256))
