7e2b0739d7f4eaeac6c76e9883ef2cb04483366d
Wes Barnett <wes@barnettphd.com>
2021-12-07 07:20:26 -0500

    functools

--- day7/main.py
+++ day7/main.py
@@ -1,19 +1,19 @@
 from argparse import ArgumentParser
 from collections import Counter
+from functools import partial
 from pathlib import Path
 
 
-def calc_fuel(positions, new_position):
-    return sum(abs(pos - new_position)*num for pos, num in positions.items())
+def fuel_func1(x1, x2):
+    return abs(x1 - x2)
 
 
-def calc_fuel_part2(positions, new_position):
+def fuel_func2(x1, x2, fuel_map):
+    return fuel_map[fuel_func1(x1, x2)]
 
-    total_fuel = 0
-    for pos, num in positions.items():
-        fuel = sum(range(1, abs(pos - new_position)+1))
-        total_fuel += fuel*num
-    return total_fuel
+
+def calc_fuel(positions, new_position, fuel_func):
+    return sum(fuel_func(pos, new_position)*num for pos, num in positions.items())
 
 
 if __name__ == "__main__":
@@ -23,6 +23,13 @@ if __name__ == "__main__":
 
     positions = Counter([int(x) for x in Path(args.infile).read_text().rstrip("\n").split(",")])
 
-    print(min(calc_fuel(positions, x) for x in range(min(positions.keys()), max(positions.keys())+1)))
+    start, end = min(positions.keys()), max(positions.keys())+1
 
-    print(min(calc_fuel_part2(positions, x) for x in range(min(positions.keys()), max(positions.keys())+1)))
+    fuel_map = {0: 0}
+    for x in range(1, end):
+        fuel_map[x] = fuel_map[x-1]+x
+
+    fuel_func_mapped = partial(fuel_func2, fuel_map=fuel_map)
+
+    for f in fuel_func1, fuel_func_mapped:
+        print(min(calc_fuel(positions, x, f) for x in range(start, end)))
