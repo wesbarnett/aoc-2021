5bf88e5ace691f93b54fddfd542c632aa8f1be83
Wes Barnett <wes@barnettphd.com>
2021-12-07 06:56:33 -0500

    day 7 part 2

--- day7/main.py
+++ day7/main.py
@@ -4,10 +4,14 @@ from pathlib import Path
 
 
 def calc_fuel(positions, new_position):
+    return sum(abs(pos - new_position)*num for pos, num in positions.items())
+
+
+def calc_fuel_part2(positions, new_position):
 
     total_fuel = 0
     for pos, num in positions.items():
-        fuel = abs(pos - new_position)
+        fuel = sum(range(1, abs(pos - new_position)+1))
         total_fuel += fuel*num
     return total_fuel
 
@@ -18,4 +22,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     positions = Counter([int(x) for x in Path(args.infile).read_text().rstrip("\n").split(",")])
+
     print(min(calc_fuel(positions, x) for x in range(min(positions.keys()), max(positions.keys())+1)))
+
+    print(min(calc_fuel_part2(positions, x) for x in range(min(positions.keys()), max(positions.keys())+1)))
