b3bfe50f5dde12f48d87f2f6a223417794bb3932
Wes Barnett <wes@barnettphd.com>
2021-12-06 19:03:24 -0500

    day 6 golang

---
+++ day6/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func countFish(line []string, days int) int {
+
+	fish := make([]int, 9)
+
+	for _, x := range line {
+		i, _ := strconv.Atoi(x)
+		fish[i] += 1
+	}
+
+	for i := 0; i < days; i++ {
+		fish = append(fish[1:], fish[0])
+		fish[6] += fish[8]
+	}
+
+	sum := 0
+	for _, x := range fish {
+		sum += x
+	}
+
+	return sum
+
+}
+
+func main() {
+
+	var file string
+	flag.StringVar(&file, "infile", "input", "Input file")
+	flag.Parse()
+
+	content, err := os.ReadFile(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	line := strings.Split(strings.Trim(string(content), "\n"), ",")
+	fmt.Println(countFish(line, 80))
+	fmt.Println(countFish(line, 256))
+
+}
