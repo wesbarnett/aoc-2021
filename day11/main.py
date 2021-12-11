eaa4ab685eb333a80b6eadd94dbaae9c74af8d59
Wes Barnett <wes@barnettphd.com>
2021-12-11 11:46:52 -0500

    refactor

--- day11/main.py
+++ day11/main.py
@@ -1,17 +1,27 @@
 from argparse import ArgumentParser
+from collections import Counter
 from pathlib import Path
 
 
-def flash_sequence(i, j, data, flashed):
+def is_valid_point(x, y, data):
+    return x >= 0 and x < len(data) and y >= 0 and y < len(data[x])
+
+
+def neighbors(i, j, data):
+    for x in range(i-1, i+2):
+        for y in range(j-1, j+2):
+            if (x, y) != (i, j) and is_valid_point(x, y, data):
+                yield x, y
+
+
+def do_flashing(i, j, data, flashed):
 
     def flash(i, j):
         if data[i][j] > 9 and (i, j) not in flashed:
             flashed.add((i, j))
-            for x in range(i-1, i+2):
-                for y in range(j-1, j+2):
-                    if (x, y) != (i, j) and x >= 0 and x < len(data) and y >= 0 and y < len(data[x]):
-                        data[x][y] += 1
-                        flash(x, y)
+            for x, y in neighbors(i, j, data):
+                data[x][y] += 1
+                flash(x, y)
 
     flash(i, j)
 
@@ -25,30 +35,27 @@ if __name__ == "__main__":
     lines = Path(args.infile).read_text().rstrip("\n").split("\n")
 
     data = [[int(x) for x in row] for row in lines]
+    size = len(data)*len(data[0])
 
-    flashes = 0
-    step = 0
-    while True:
-        flashed = set()
+    flashes = Counter()
+    step = 1
+    flashed = set()
+
+    while len(list(flashed)) != size:
 
         data = [[x + 1 for x in row] for row in data]
 
+        flashed = set()
         for i, row in enumerate(data):
-            for j, x in enumerate(row):
-                flash_sequence(i, j, data, flashed)
+            for j, _ in enumerate(row):
+                do_flashing(i, j, data, flashed)
 
-        # Part 2 - first step all fish flash at same time
-        if len(list(flashed)) == len(data[0])*len(data):
-            all_flash_step = step+1
-            break
-
+        flashes[step] += flashes[step-1]
         for x, y in list(flashed):
             data[x][y] = 0
-            # Part 1 - how many flashes after 100 steps
-            if step < 100:
-                flashes += 1
+            flashes[step] += 1
 
         step += 1
 
-    print(flashes)
-    print(all_flash_step)
+    print(f"Part 1 result: {flashes[100]}")
+    print(f"Part 2 result: {step-1}")
