c4b8bda89cbb1cae9e1d726e6a16d40ef8c07bce
Wes Barnett <wes@barnettphd.com>
2021-12-12 12:57:05 -0500

    refactor

--- day12/main.py
+++ day12/main.py
@@ -3,41 +3,7 @@ from collections import Counter, defaultdict
 from pathlib import Path
 
 
-def traverse(node, graph):
-
-    visited = set(["start"])
-
-    def visit(node):
-
-        if node == "end":
-            return 1
-
-        if node.islower():
-            visited.add(node)
-
-        s = 0
-        for dst in graph[node]:
-            if dst not in visited:
-                s += visit(dst)
-
-        if node in visited:
-            visited.remove(node)
-
-        return s
-
-    result = visit(node)
-    return result
-
-
-def visited_small_cave_twice(visited):
-    return visited.most_common()[0][1] == 2
-
-
-def can_visit_cave(visited, dst):
-    return dst != "start" and (not visited_small_cave_twice(visited) and visited[dst] < 2) or (visited_small_cave_twice and visited[dst] < 1)
-
-
-def traverse2(node, graph):
+def traverse(graph, can_visit_cave_func):
 
     visited = Counter()
 
@@ -49,18 +15,29 @@ def traverse2(node, graph):
         if node.islower():
             visited[node] += 1
 
-        s = 0
-        for dst in graph[node]:
-            if can_visit_cave(visited, dst):
-                s += visit(dst)
+        s = sum(visit(dst) for dst in graph[node] if can_visit_cave_func(visited, dst))
 
-        if node.islower() and visited[node] > 0:
+        if visited[node] > 0:
             visited[node] -= 1
 
         return s
 
-    result = visit(node)
-    return result
+    return visit("start")
+
+
+def visited_small_cave_twice(visited):
+    return visited.most_common()[0][1] == 2
+
+
+def can_visit_cave(visited, dst):
+    return dst != "start" and visited[dst] == 0
+
+
+def can_visit_cave2(visited, dst):
+    return dst != "start" and (
+        (not visited_small_cave_twice(visited) and visited[dst] < 2) or
+        (visited_small_cave_twice and visited[dst] < 1)
+    )
 
 
 if __name__ == "__main__":
@@ -79,6 +56,5 @@ if __name__ == "__main__":
         if src != "start" and dst != "end":
             graph[dst].add(src)
 
-    print(graph)
-    print(traverse("start", graph))
-    print(traverse2("start", graph))
+    print(traverse(graph, can_visit_cave))
+    print(traverse(graph, can_visit_cave2))
