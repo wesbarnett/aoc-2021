1282839ca47a8b2fbcd3193f4032a199237c6de7
Wes Barnett <wes@barnettphd.com>
2021-12-12 12:48:01 -0500

    day 12 part 2

--- day12/main.py
+++ day12/main.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-from collections import defaultdict
+from collections import Counter, defaultdict
 from pathlib import Path
 
 
@@ -29,6 +29,40 @@ def traverse(node, graph):
     return result
 
 
+def visited_small_cave_twice(visited):
+    return visited.most_common()[0][1] == 2
+
+
+def can_visit_cave(visited, dst):
+    return dst != "start" and (not visited_small_cave_twice(visited) and visited[dst] < 2) or (visited_small_cave_twice and visited[dst] < 1)
+
+
+def traverse2(node, graph):
+
+    visited = Counter()
+
+    def visit(node):
+
+        if node == "end":
+            return 1
+
+        if node.islower():
+            visited[node] += 1
+
+        s = 0
+        for dst in graph[node]:
+            if can_visit_cave(visited, dst):
+                s += visit(dst)
+
+        if node.islower() and visited[node] > 0:
+            visited[node] -= 1
+
+        return s
+
+    result = visit(node)
+    return result
+
+
 if __name__ == "__main__":
 
     parser = ArgumentParser()
@@ -45,4 +79,6 @@ if __name__ == "__main__":
         if src != "start" and dst != "end":
             graph[dst].add(src)
 
+    print(graph)
     print(traverse("start", graph))
+    print(traverse2("start", graph))
