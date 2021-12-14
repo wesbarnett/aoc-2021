85a052cfc3ec5c5b8219384f34000211aca33349
Wes Barnett <wes@barnettphd.com>
2021-12-14 16:04:12 -0500

    fix day 14, get part 2

--- day14/main.py
+++ day14/main.py
@@ -3,37 +3,60 @@ from collections import Counter
 from pathlib import Path
 
 
-class Node:
+def init_pair_counts(template):
 
-    def __init__(self, val):
-        self.val = val
-        self.next = None
+    pair_counts = Counter()
+    for i in range(len(polymer_template[:-1])):
+        pair_counts[polymer_template[i:i+2]] += 1
 
-    def __repr__(self):
-        return f"Node(val={self.val})"
-
-
-def create_starting_polymer(polymer_template):
-    head = Node(polymer_template[0])
-    node = head
-    for i in range(1, len(polymer_template)):
-        node.next = Node(polymer_template[i])
-        node = node.next
-    return head
+    return pair_counts
 
 
-def insert_elements(head, rules):
+def update_pair_counts(pair_counts, rules):
+    """
+    When an element is inserted between a pair, that pair is eliminated and the new
+    pairs are added.
+    """
 
-    node = head
-    while node:
-        for rule in rules:
-            pair, ins = rule.split(" -> ")
-            new_node = Node(ins)
-            if node.val == pair[0] and node.next and node.next.val == pair[1]:
-                new_node.next, node.next = node.next, new_node
-                node = new_node
-                break
-        node = node.next
+    # Keep a separate copy of the counter because the rules are applied at the same time
+    new_pair_counts = Counter(pair_counts)
+    for rule in rules:
+        pair, ins = rule.split(" -> ")
+        if pair_counts[pair] > 0:
+            new_pair_counts[pair[0]+ins] += pair_counts[pair]
+            new_pair_counts[ins+pair[1]] += pair_counts[pair]
+            new_pair_counts[pair] -= pair_counts[pair]
+    return Counter(new_pair_counts)
+
+
+def run(steps, polymer_template, rules):
+    pair_counts = init_pair_counts(polymer_template)
+
+    for _ in range(steps):
+        pair_counts = update_pair_counts(pair_counts, rules)
+
+    return get_element_counts(pair_counts)
+
+
+def get_element_counts(pair_counts):
+    """
+    Count each starting element and ending element for each pair. These should be
+    exactly the same numbers for each element except for the starting pair and the
+    ending pair (which will be off by one). Thus we take the max for each element from
+    those two counts.
+    """
+
+    counts1 = Counter()
+    counts2 = Counter()
+    for k, v in pair_counts.items():
+        counts1[k[0]] += v
+        counts2[k[1]] += v
+
+    counts = Counter()
+    for k in counts1.keys():
+        counts[k] = max(counts1[k], counts2[k])
+
+    return counts
 
 
 if __name__ == "__main__":
@@ -44,17 +67,10 @@ if __name__ == "__main__":
 
     lines = Path(args.infile).read_text().rstrip("\n").split("\n")
     polymer_template = lines[0]
-    pair_insertion_rules = lines[2:]
+    rules = lines[2:]
 
-    head = create_starting_polymer(polymer_template)
+    counts = run(10, polymer_template, rules)
+    print(counts.most_common()[0][1]-counts.most_common()[-1][1])
 
-    for _ in range(10):
-        insert_elements(head, pair_insertion_rules)
-
-    counter = Counter()
-    node = head
-    while node:
-        counter[node.val] += 1
-        node = node.next
-
-    print(counter.most_common()[0][1] - counter.most_common()[-1][1])
+    counts = run(40, polymer_template, rules)
+    print(counts.most_common()[0][1]-counts.most_common()[-1][1])
