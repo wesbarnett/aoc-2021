18846baef7d61db619b5cdbb67665c10976f0c00
Wes Barnett <wes@barnettphd.com>
2021-12-15 07:41:41 -0500

    refactor

--- day15/main.py
+++ day15/main.py
@@ -2,13 +2,8 @@ from argparse import ArgumentParser
 from pathlib import Path
 
 
-if __name__ == "__main__":
+def find_lowest_risk(data):
 
-    parser = ArgumentParser()
-    parser.add_argument("-i", "--infile", required=False, type=Path, default=Path("input"))
-    args = parser.parse_args()
-
-    data = [[int(x) for x in row] for row in Path(args.infile).read_text().rstrip("\n").split("\n")]
     nrows = len(data)
     ncols = len(data[0])
 
@@ -26,4 +21,15 @@ if __name__ == "__main__":
         for col in range(1, ncols):
             risk[row][col] += data[row][col] + min(risk[row-1][col], risk[row][col-1])
 
-    print(risk[-1][-1])
+    return risk[-1][-1]
+
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser()
+    parser.add_argument("-i", "--infile", required=False, type=Path, default=Path("input"))
+    args = parser.parse_args()
+
+    data = [[int(x) for x in row] for row in Path(args.infile).read_text().rstrip("\n").split("\n")]
+
+    print(find_lowest_risk(data))
