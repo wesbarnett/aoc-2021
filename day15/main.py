5c0c98c4298aa39a148878c79e62f19a2faeeec4
Wes Barnett <wes@barnettphd.com>
2021-12-15 13:30:50 -0500

    refactor

--- day15/main.py
+++ day15/main.py
@@ -2,16 +2,15 @@ from argparse import ArgumentParser
 from pathlib import Path
 
 
-def find_lowest_risk(data, dup=1):
+def tile_data(data, n):
 
     nrows_orig = len(data)
     ncols_orig = len(data[0])
 
-    nrows = nrows_orig*dup
-    ncols = ncols_orig*dup
+    ncols = len(data[0])*n
+    nrows = len(data)*n
 
-    risk = [[0 for _ in range(ncols)] for _ in range(nrows)]
-    data_tmp = [[[0 for _ in range(ncols_orig)] for _ in range(nrows_orig)] for _ in range(dup*2)]
+    data_tmp = [[[0 for _ in range(ncols_orig)] for _ in range(nrows_orig)] for _ in range(n*2)]
     data_tiled = [[0 for _ in range(ncols)] for _ in range(nrows)]
 
     for row in range(nrows_orig):
@@ -19,7 +18,7 @@ def find_lowest_risk(data, dup=1):
             data_tiled[row][col] = data[row][col]
             data_tmp[0][row][col] = data[row][col]
 
-    for i in range(1, dup*2):
+    for i in range(1, n*2):
 
         for row in range(nrows_orig):
             for col in range(ncols_orig):
@@ -28,23 +27,33 @@ def find_lowest_risk(data, dup=1):
                     val = 9
                 data_tmp[i][row][col] = val
 
-    for j in range(dup):
-        for i in range(dup):
+    for j in range(n):
+        for i in range(n):
             for row in range(nrows_orig):
                 for col in range(ncols_orig):
                     data_tiled[row+nrows_orig*i][col+ncols_orig*j] = data_tmp[i+j][row][col]
 
+    return data_tiled
+
+
+def find_lowest_risk(data, dup=1):
+
+    data = tile_data(data, dup)
+    ncols = len(data[0])
+    nrows = len(data)
+
+    risk = [[0 for _ in range(ncols)] for _ in range(nrows)]
     risk[0][0] = 0
 
     for row in range(1, nrows):
-        risk[row][0] = data_tiled[row][0] + risk[row-1][0]
+        risk[row][0] = data[row][0] + risk[row-1][0]
 
     for col in range(1, ncols):
-        risk[0][col] = data_tiled[0][col] + risk[0][col-1]
+        risk[0][col] = data[0][col] + risk[0][col-1]
 
     for row in range(1, nrows):
         for col in range(1, ncols):
-            risk[row][col] += data_tiled[row][col] + min(risk[row-1][col], risk[row][col-1])
+            risk[row][col] += data[row][col] + min(risk[row-1][col], risk[row][col-1])
 
     return risk[-1][-1]
 
