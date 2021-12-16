8d2b5ae0778a32d59bd71cf3245b7df694a63b8d
Wes Barnett <wes@barnettphd.com>
2021-12-16 07:39:16 -0500

    progress

--- day16/main.py
+++ day16/main.py
@@ -8,8 +8,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     data = Path(args.infile).read_text().rstrip("\n")
-    x = "D2FE28"
-    b = bin(int(x, 16))[2:]
+    # x = "D2FE28"
+    x = "38006F45291200"
+    b = format(int(x, 16), f"0>{len(x)*4}b")
 
     i = 0
     vers = int(b[i:i+3], 2)
@@ -28,4 +29,13 @@ if __name__ == "__main__":
             i += 5
         g = b[i+1:i+5]
         groups.append(g)
-        result = int("".join(groups), 2))
+        result = int("".join(groups), 2)
+        print(result)
+    else:
+        length_type_id = b[i]
+        if length_type_id == "0":
+            i += 1
+            packet_length = int(b[i:i+15], 2)
+            print(packet_length)
+        else:
+            pass
