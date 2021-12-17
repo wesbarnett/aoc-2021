3526cfe49963efec3f7949bebba5c9e79ff7433e
Wes Barnett <wes@barnettphd.com>
2021-12-17 16:28:45 -0500

    update

--- day17/main.py
+++ day17/main.py
@@ -1,27 +1,17 @@
 
 tx1, tx2, ty1, ty2 = 211, 232, -124, -69
-
-maxy = 0
-
-result = []
+maxy = count = 0
 for vxi in range(1, tx2+1):
     for vyi in range(ty1, abs(ty1)):
-        x, y = 0, 0
-        vx, vy = vxi, vyi
+        x, y, vx, vy = 0, 0, vxi, vyi
         maxy_tmp = 0
-        while x <= tx2 and ty1 <= y and (vx > 0 or x > -tx1):
-            x += vx
-            y += vy
-            if vx > 0:
-                vx -= 1
-            vy -= 1
-            if y > maxy_tmp:
-                maxy_tmp = y
+        while x <= tx2 and ty1 <= y and (vx > 0 or x > tx1):
+            x, y, vx, vy = x + vx, y + vy, vx - 1 if vx > 0 else vx, vy - 1
+            maxy_tmp = max(y, maxy_tmp)
             if tx1 <= x <= tx2 and ty1 <= y <= ty2:
-                if maxy_tmp > maxy:
-                    maxy = maxy_tmp
-                result.append((vxi, vyi))
+                maxy = max(maxy, maxy_tmp)
+                count += 1
                 break
 
 print(maxy)
-print(len(result))
+print(count)
