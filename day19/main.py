1ca6341f5536727241bb11b080cf526df809171b
Wes Barnett <wes@barnettphd.com>
2021-12-29 16:40:45 -0500

    update

--- day19/main.py
+++ day19/main.py
@@ -1,7 +1,29 @@
 from argparse import ArgumentParser
 from collections import defaultdict
+from itertools import permutations, product
 from pathlib import Path
 
+def search(scanners, j, offset):
+    for k in range(len(scanners[j])):
+        x0, y0, z0 = scanners[j][k]
+        for i in range(len(scanners.keys())):
+            if i != j:
+                for s in product([-1, 1], repeat=3):
+                    for d in permutations(range(3)):
+                        for p in scanners[i]:
+                            x1, y1, z1 = s[0]*p[d[0]], s[1]*p[d[1]], s[2]*p[d[2]]
+                            dx, dy, dz = x0 - x1, y0 - y1, z0 - z1
+                            beacons = []
+                            for p in scanners[i]:
+                                beacon = s[0]*p[d[0]] + dx, s[1]*p[d[1]] + dy, s[2]*p[d[2]] + dz
+                                if beacon in scanners[j]:
+                                    beacons.append(beacon)
+                            if len(beacons) >= 12:
+#                               for beacon in beacons:
+#                                   all_beacons.add(beacon)
+                                offset[(j, i)] = tuple((dx, dy, dz))
+                                return
+    
 
 if __name__ == "__main__":
 
@@ -19,28 +41,10 @@ if __name__ == "__main__":
         elif line:
             scanners[i].append(tuple(map(int, line.split(","))))
 
-    all_beacons = set()
     offset = {}
 
     for j in range(5):
-        x0, y0, z0 = scanners[j][0]
-        for i in range(5):
-            if i != j:
-                for s in [(1, 1, 1), (-1, 1, 1), (1, -1, 1), (1, 1, -1), (-1, -1, 1), (-1, 1, -1), (1, -1, -1), (-1, -1, -1)]:
-                    for d in [(0, 1, 2), (0, 2, 1), (1, 0, 2), (1, 2, 0), (2, 0, 1), (2, 1, 0)]:
-                        for p in scanners[i]:
-                            x1, y1, z1 = s[0]*p[d[0]], s[1]*p[d[1]], s[2]*p[d[2]]
-                            dx, dy, dz = x0 - x1, y0 - y1, z0 - z1
-                            beacons = []
-                            for p in scanners[i]:
-                                beacon = s[0]*p[d[0]] + dx, s[1]*p[d[1]] + dy, s[2]*p[d[2]] + dz
-                                if beacon in scanners[j]:
-                                    beacons.append(beacon)
-                            if len(beacons) >= 12:
-#                               for beacon in beacons:
-#                                   all_beacons.add(beacon)
-                                print(s, d)
-                                offset[(j, i)] = tuple((dx, dy, dz))
+        search(scanners, j, offset)
 
     print(offset)
 #   print(all_beacons)
